6c5b4c157201a06fe981eef616b029f0ec1034fe
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-09-10 00:16:38 -0600

    added jQuery scrolling

--- javascript/trash-script.js
+++ javascript/trash-script.js
@@ -7,18 +7,33 @@ img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-}
+};
 
 var span = document.getElementsByClassName("close")[0];
-
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
+
+// scroll to sign petition button
+$(document).ready(function() {
+    $('#sign').click(function(){
+        $('html, body').animate({scrollTop: $("#cta1").offset().top
+      }, 'slow', 'swing');
+    });
+});
+
+// scroll to sign petition button mobile
+$(document).ready(function() {
+    $('#sign1').click(function(){
+        $('html, body').animate({scrollTop: $("#cta1").offset().top
+      }, 'slow', 'swing');
+    });
+});
 
 // scroll to content start
 $(document).ready(function() {
     $('#arrows-down').click(function(){
-        $('html, body').animate({scrollTop:570}, 'medium', 'swing');
-        return false;
+        $('html, body').animate({scrollTop: $("#my-content").offset().top
+      }, 'slow', 'swing');
     });
 });
