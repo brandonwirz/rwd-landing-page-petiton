cba282e8efceb76d3ce4a2650ed21079cec7b2b6
Brandon Wirz <brandonwirz@Brandons-MacBook-Pro.local>
2018-09-11 01:47:37 -0600

    consolidating jQuery code and CSS tweaks

--- javascript/trash-script.js
+++ javascript/trash-script.js
@@ -14,24 +14,17 @@ span.onclick = function() {
     modal.style.display = "none";
 };
 
-// scroll to sign petition button
+// scroll to sign petition button or content start
 $(document).ready(function() {
     $('#sign').click(function(){
         $('html, body').animate({scrollTop: $("#cta1").offset().top
       }, 'slow', 'swing');
     });
-});
-
-// scroll to sign petition button mobile
-$(document).ready(function() {
     $('#sign1').click(function(){
         $('html, body').animate({scrollTop: $("#cta1").offset().top
       }, 'slow', 'swing');
     });
-});
-
-// scroll to content start
-$(document).ready(function() {
+    // scroll to content start
     $('#arrows-down').click(function(){
         $('html, body').animate({scrollTop: $("#my-content").offset().top
       }, 'slow', 'swing');
